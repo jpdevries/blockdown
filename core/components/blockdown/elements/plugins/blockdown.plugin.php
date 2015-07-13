83209d3ac9e282a32490ed1e6bb1612d079be4f8
JP DeVries <johnpaul.devries@gmail.com>
2015-07-13 12:36:28 -0700

    Removing Stray Debug Statement
    Closes #3

--- core/components/blockdown/elements/plugins/blockdown.plugin.php
+++ core/components/blockdown/elements/plugins/blockdown.plugin.php
@@ -8,7 +8,6 @@ if ($modx->event->name == 'ContentBlocks_RegisterInputs') {
     // Load your own class. No need to require cbBaseInput, that's already loaded.
     $path = $modx->getOption('blockdown.core_path', null, MODX_CORE_PATH . 'components/blockdown/');
     require_once($path . 'elements/inputs/blockdown.class.php');
-    $modx->log(modX::LOG_LEVEL_ERROR,$path . 'elements/inputs/blockdown.class.php');
     $opts = array('assetsUrl' => $modx->getOption('blockdown.assets_url', null, $modx->getOption('assets_url',null,'assets/') . 'components/blockdown/'));
     $modx->regClientStartupHTMLBlock('<script>var ContentBlocksBlockDown = ' . $modx->toJson($opts) . '</script>');
     // Create an instance of your input type, passing the $contentBlocks var
